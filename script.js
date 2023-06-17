383b622b821acf3947fbac23d5e88b5f794c6cb9
yungshun317 <seraph317@gmail.com>
2023-06-18 01:52:32 +0800

    [FEAT] Animate multiple targets with the `stagger` property

--- script.js
+++ script.js
@@ -21,25 +21,27 @@ function Triangle(id, x_1, y_1, x_2, y_2, x_3, y_3, fill) {
             + this.y_3 + " "
             + this.x_1 + " "
             + this.y_1
-            + `" fill="${this.fill}"/>`;
+            + `" fill="${this.fill}" />`;
     }
 }
 
 function assembleTriangles() {
     let y1 = 60.6, y2 = 70.7, y3 = 70.7;
     let quantRow = 15, quantColumn = 27;
-    let stepX=30, stepY=30;
+    let stepX= 30, stepY= 30;
     let i = 0;
-    
+
     for (let j = 0; j < quantRow; j++) {
         let x1 = 77.9, x2 = 61.7, x3 = 94;
         let angle = -90;
+
         for (let k = 0; k < quantColumn; k++) {
             i++;
             new Triangle("trg" + i, x1, y1, x2, y2, x3, y3, "yellow").draw();
             gsap.set("#trg" + i, {
-                transformOrigin:"-50% -50%",
-                rotate: angle
+                transformOrigin: "-50% -50%",
+                transformBox: "view-box",
+                rotation: angle
             });
             angle += 13.3;
             x1 += stepX;
@@ -54,15 +56,15 @@ function assembleTriangles() {
 
 assembleTriangles();
 
-/*
 document.addEventListener("click", function(e) {
     gsap.to(".trg", {
+        rotation: 360000,
         duration: 5000,
-        rotate: 360000,
         repeat: -1,
         ease: "linear",
-        stagger: -0.005
+        stagger: {
+            each: -0.005,
+            // from: "end"
+        }
     });
 });
-*/
- */
