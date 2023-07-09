d7aedebc60a35ff1475baec0622d0dd82f8dd076
yungshun317 <seraph317@gmail.com>
2023-07-09 17:42:05 +0800

    [FEAT] Crop the image into equal parts and load them respectively into the grid of `canvas`

--- rotating-code/script.js
+++ rotating-code/script.js
@@ -14,17 +14,17 @@ function loadCanvas(curCanvasId) {
 function loadImg(curCanvasId, i) {
     let canvas = document.getElementById(curCanvasId);
     let context = canvas.getContext("2d");
-    let imageObj = new Image();
+    let imageObj= new Image();
 
     imageObj.onload = function() {
         let sourceX = 0;
         let sourceY = i;
-        let sourceWidth = 946;
-        let sourceHeight = 28;
-        let destWidth = sourceWidth;
-        let destHeight = 28;
-        let destX = 0;
-        let destY = 0;
+        let sourceWidth= 946;
+        let sourceHeight= 28;
+        let destWidth= sourceWidth;
+        let destHeight= 28;
+        let destX= 0;
+        let destY= 0;
 
         context.drawImage(imageObj, sourceX, sourceY, sourceWidth, sourceHeight, destX, destY, destWidth, destHeight);
     }
@@ -33,8 +33,34 @@ function loadImg(curCanvasId, i) {
 }
 
 for (let i = 0; i < 27; i++) {
-    let curCanvasId="canvas_Column" + i;
+    let curCanvasId= "canvas_Column" + i;
 
     loadCanvas(curCanvasId);
-    loadImg(curCanvasId,i * 28);
+    loadImg(curCanvasId, i * 28);
 };
+
+let skip= 1, increment= 2;
+let angleY= 0, angleZ= 0;
+
+window.addEventListener("mousemove", function(e) {
+    if (skip % 5 == 0) {
+        if (angleY < -89) {
+            if (angleY % 90 == 0) {
+                increment = increment * (-1);
+            }
+        }
+
+        gsap.to(".block", {
+            duration: 2,
+            rotateY: angleY,
+            rotateZ: -angleZ,
+            stagger: 0.1
+        });
+
+        skip += 1;
+        angleY = angleY - 5 * Math.abs(increment);
+        angleZ = angleZ + increment;
+    }
+
+    skip += 1;
+}, false);
